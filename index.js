2ed85b7753a7cfe62c4101439b6b299af437b374
Robert Susewell <susewell@live.unc.edu>
2022-04-29 16:31:21 -0400

    added help text

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Place your server entry point code here
 const args = require('minimist')(proccess.argv.slice(2))
+
+const help = (`
+server.js [options]
+--port, -p	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help, -h	Return this message and exit.
+`)
